52d12f050441df43da5bed16c0a5cc965c1c8698
MaheshPadashetti <maheepadashetti76@gmail.com>
2020-04-23 17:57:15 +0530

    [Mahesh]Implemented toolbar component

--- BookStore/src/app/app-routing.module.ts
+++ BookStore/src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './Component/home/home.component';
+import { ToolbarComponent } from './Component/toolbar/toolbar.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"home",component:HomeComponent},
+  {path:"",component:ToolbarComponent},
+  {path:"toolbar",component:ToolbarComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- BookStore/src/app/app.module.ts
+++ BookStore/src/app/app.module.ts
@@ -4,15 +4,23 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './Component/home/home.component';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { ToolbarComponent } from './Component/toolbar/toolbar.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    ToolbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    
+    
+
   ],
   providers: [],
   bootstrap: [AppComponent]
